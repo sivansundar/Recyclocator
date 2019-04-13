67806d45e78e9d56076f0e93b8627ee270abe24b
Sivan <sivan.sundar@gmail.com>
2019-04-13 20:45:53 +0530

    Camera detection

--- app/src/main/java/com/kinomisfit/recyclocator/CameraActivity.java
+++ app/src/main/java/com/kinomisfit/recyclocator/CameraActivity.java
@@ -1,6 +1,10 @@
 package com.kinomisfit.recyclocator;
 
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
@@ -8,6 +12,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.camerakit.CameraKitView;
 import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
+import com.google.firebase.ml.common.FirebaseMLException;
+import com.google.firebase.ml.common.modeldownload.FirebaseLocalModel;
+import com.google.firebase.ml.common.modeldownload.FirebaseModelDownloadConditions;
+import com.google.firebase.ml.common.modeldownload.FirebaseModelManager;
+import com.google.firebase.ml.custom.FirebaseModelDataType;
+import com.google.firebase.ml.custom.FirebaseModelInputOutputOptions;
+import com.google.firebase.ml.custom.FirebaseModelInterpreter;
+import com.google.firebase.ml.custom.FirebaseModelOptions;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -15,10 +37,61 @@ import butterknife.OnClick;
 
 public class CameraActivity extends AppCompatActivity {
 
+    private static final String TAG = "Camera Activity : ";
     @BindView(R.id.camera)
     CameraKitView camera;
     @BindView(R.id.fabcam)
     ExtendedFloatingActionButton fabcam;
+
+    private FirebaseModelInterpreter mInterpreter;
+
+    private FirebaseModelInputOutputOptions mDataOptions;
+
+    private List<String> mLabelList;
+
+
+    private static final String HOSTED_MODEL_NAME = "cloud_model_1";
+    private static final String LOCAL_MODEL_ASSET = "mobilenet_v1_1.0_224_quant.tflite";
+
+    private static final String LABEL_PATH = "//assets//labels.txt";
+
+
+    private static final int DIM_BATCH_SIZE = 1;
+    private static final int DIM_PIXEL_SIZE = 3;
+    private static final int DIM_IMG_SIZE_X = 224;
+    private static final int DIM_IMG_SIZE_Y = 224;
+
+    private static final int RESULTS_TO_SHOW = 3;
+
+
+
+    private final PriorityQueue<Map.Entry<String, Float>> sortedLabels =
+            new PriorityQueue<>(
+                    RESULTS_TO_SHOW,
+                    new Comparator<Map.Entry<String, Float>>() {
+                        @Override
+                        public int compare(Map.Entry<String, Float> o1, Map.Entry<String, Float>
+                                o2) {
+                            return (o1.getValue()).compareTo(o2.getValue());
+                        }
+                    });
+
+    private List<String> loadLabelList(Activity activity) {
+        List<String> labelList = new ArrayList<>();
+        try (BufferedReader reader =
+                     new BufferedReader(new InputStreamReader(activity.getAssets().open
+                             (LABEL_PATH)))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                labelList.add(line);
+            }
+        } catch (IOException e) {
+            Log.e(TAG, "Failed to read label list.", e);
+        }
+        return labelList;
+    }
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,13 +100,18 @@ public class CameraActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
 
+
     }
 
     @Override
     protected void onStart() {
         super.onStart();
         camera.onStart();
+
+       
     }
+
+
 
     @Override
     protected void onResume() {
@@ -71,6 +149,9 @@ public class CameraActivity extends AppCompatActivity {
                 //set up local model values.
                 //run the model with the bitmap
                 //obtain values and just Toast them
+
+
+
 
 
 
